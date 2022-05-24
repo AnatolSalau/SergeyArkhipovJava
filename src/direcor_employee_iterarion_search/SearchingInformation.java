c6481cd2b486befa3d32ff91ddca110648c491b2
AnatolSalau <salovanatoly5@mail.ru>
2022-05-24 20:41:53 +0300

    Added Iteration DeepSearch

--- src/direcor_employee_iterarion_search/SearchingInformation.java
+++ src/direcor_employee_iterarion_search/SearchingInformation.java
@@ -4,9 +4,6 @@ import queue_realization_generic.SimpleQueueGeneric;
 import tree_binary_reqursion_deep_search.BinaryTree;
 
 class SearchingInformation {
-/*
-* Добавил еще один метод для поиска имени только у директора,
-* и переименовал старый метод с  haveNameRecursion на isNameInArrayEmployees */
         public static boolean isNameInArrayEmployees(String name, Employee rootEmployee) {
                 //результат
                 boolean haveName = false;
@@ -14,7 +11,33 @@ class SearchingInformation {
                 SimpleQueueGeneric<Employee> stack = new SimpleQueueGeneric<Employee>(new Employee[20]);
                         //Добавляем в стэк корневой узел
                         stack.add(rootEmployee);
-
+            while (! stack.isEmpty()) {
+                //достаем из стэка корневой узел
+                Employee nodeEmployee = stack.get();
+                if (nodeEmployee.getName().equals(name)) {
+                    haveName = true;
+                }
+                //Проверяем директор или нет наш сотрудник
+                if (nodeEmployee instanceof Director) {
+                    //если директор приводим его к директору
+                    Employee[] newEmployees = ((Director) nodeEmployee).getArrayEmployees();
+                    //бежим циклом по массиву сотрудников директора
+                    for (Employee employee :newEmployees ) {
+                        //Добавляем всех найденных директоров в стэк
+                            stack.add(employee);
+                    }
+                }
+            }
+                return haveName;
+        }
+    public static boolean isNameInArrayEmployees(String name, Employee... rootEmployees) {
+        //результат
+        boolean haveName = false;
+        //эмуляция стэка
+        SimpleQueueGeneric<Employee> stack = new SimpleQueueGeneric<Employee>(new Employee[20]);
+        for (Employee rootEmployee: rootEmployees) {
+            //Добавляем в стэк корневой узел
+            stack.add(rootEmployee);
             while (! stack.isEmpty()) {
                 //достаем из стэка корневой узел
                 Employee nodeEmployee = stack.get();
@@ -30,12 +53,14 @@ class SearchingInformation {
                     //бежим циклом по массиву сотрудников директора
                     for (Employee employee :newEmployees ) {
                         //Добавляем всех найденных директоров в стэк
-                            stack.add(employee);
+                        stack.add(employee);
                     }
                 }
             }
-                return haveName;
         }
+        return haveName;
+    }
+
 
 }
 
